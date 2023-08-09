94618c5b787798185543cfb49dd75dabd6fd6e84
Ameen Ur Rehman <ameenreh2003@gmail.com>
2023-08-09 22:02:26 +0530

    code-clean

--- app/app.py
+++ app/app.py
@@ -23,104 +23,103 @@ fertilizer_dic = {
 
         <br/> 2. <i>Coffee grinds </i> – use your morning addiction to feed your gardening habit! Coffee grinds are considered a green compost material which is rich in nitrogen. Once the grounds break down, your soil will be fed with delicious, delicious nitrogen. An added benefit to including coffee grounds to your soil is while it will compost, it will also help provide increased drainage to your soil.
 
-        <br/>3. <i>Plant nitrogen fixing plants</i> – planting vegetables that are in Fabaceae family like peas, beans and soybeans have the ability to increase nitrogen in your soil
+        <br/> 3. <i>Plant nitrogen fixing plants</i> – planting vegetables that are in Fabaceae family like peas, beans and soybeans have the ability to increase nitrogen in your soil
 
-        <br/>4. Plant ‘green manure’ crops like cabbage, corn and brocolli
+        <br/> 4. Plant ‘green manure’ crops like cabbage, corn and brocolli
 
-        <br/>5. <i>Use mulch (wet grass) while growing crops</i> - Mulch can also include sawdust and scrap soft woods""",
+        <br/> 5. <i>Use mulch (wet grass) while growing crops</i> - Mulch can also include sawdust and scrap soft woods""",
 
         'Nlow': """The N value of your soil is low.
         <br/> Please consider the following suggestions:
         <br/><br/> 1. <i>Add sawdust or fine woodchips to your soil</i> – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.
 
-        <br/>2. <i>Plant heavy nitrogen feeding plants</i> – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.
+        <br/> 2. <i>Plant heavy nitrogen feeding plants</i> – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.
 
-        <br/>3. <i>Water</i> – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.
+        <br/> 3. <i>Water</i> – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.
 
-        <br/>4. <i>Sugar</i> – In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.
+        <br/> 4. <i>Sugar</i> – In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.
 
-        <br/>5. Add composted manure to the soil.
+        <br/> 5. Add composted manure to the soil.
 
-        <br/>6. Plant Nitrogen fixing plants like peas or beans.
+        <br/> 6. Plant Nitrogen fixing plants like peas or beans.
 
-        <br/>7. <i>Use NPK fertilizers with high N value.
+        <br/> 7. <i>Use NPK fertilizers with high N value.
 
-        <br/>8. <i>Do nothing</i> – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.""",
+        <br/> 8. <i>Do nothing</i> – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.""",
 
         'PHigh': """The P value of your soil is high.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Avoid adding manure</i> – manure contains many key nutrients for your soil but typically including high levels of phosphorous. Limiting the addition of manure will help reduce phosphorus being added.
+        <br/><br/> 1. <i>Avoid adding manure</i> – manure contains many key nutrients for your soil but typically including high levels of phosphorous. Limiting the addition of manure will help reduce phosphorus being added.
 
-        <br/>2. <i>Use only phosphorus-free fertilizer</i> – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
+        <br/> 2. <i>Use only phosphorus-free fertilizer</i> – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
 
-        <br/>3. <i>Water your soil</i> – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.
+        <br/> 3. <i>Water your soil</i> – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.
 
-        <br/>4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
+        <br/> 4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
 
-        <br/>5. Use crop rotations to decrease high phosphorous levels""",
+        <br/> 5. Use crop rotations to decrease high phosphorous levels""",
 
         'Plow': """The P value of your soil is low.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Bone meal</i> – a fast acting source that is made from ground animal bones which is rich in phosphorous.
+        <br/><br/> 1. <i>Bone meal</i> – a fast acting source that is made from ground animal bones which is rich in phosphorous.
 
-        <br/>2. <i>Rock phosphate</i> – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants can use.
+        <br/> 2. <i>Rock phosphate</i> – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants can use.
 
-        <br/>3. <i>Phosphorus Fertilizers</i> – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
+        <br/> 3. <i>Phosphorus Fertilizers</i> – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
 
-        <br/>4. <i>Organic compost</i> – adding quality organic compost to your soil will help increase phosphorous content.
+        <br/> 4. <i>Organic compost</i> – adding quality organic compost to your soil will help increase phosphorous content.
 
-        <br/>5. <i>Manure</i> – as with compost, manure can be an excellent source of phosphorous for your plants.
+        <br/> 5. <i>Manure</i> – as with compost, manure can be an excellent source of phosphorous for your plants.
 
-        <br/>6. <i>Clay soil</i> – introducing clay particles into your soil can help retain & fix phosphorus deficiencies.
+        <br/> 6. <i>Clay soil</i> – introducing clay particles into your soil can help retain & fix phosphorus deficiencies.
 
-        <br/>7. <i>Ensure proper soil pH</i> – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.
+        <br/> 7. <i>Ensure proper soil pH</i> – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.
 
-        <br/>8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in increasing the pH value of the soil.
+        <br/> 8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in increasing the pH value of the soil.
 
-        <br/>9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers, such as ammonium sulfate, can help lower soil pH""",
+        <br/> 9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers, such as ammonium sulfate, can help lower soil pH""",
 
         'KHigh': """The K value of your soil is high</b>.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Loosen the soil</i> deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and repeat digging and watering the soil two or three more times.
+        <br/><br/> 1. <i>Loosen the soil</i> deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and repeat digging and watering the soil two or three more times.
 
-        <br/>2. <i>Sift through the soil</i>, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and feldspar slowly release potassium into the soil slowly through weathering.
+        <br/> 2. <i>Sift through the soil</i>, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and feldspar slowly release potassium into the soil slowly through weathering.
 
-        <br/>3. Stop applying potassium-rich commercial fertilizer. Apply only commercial fertilizer that has a '0' in the final number field. Commercial fertilizers use a three number system for measuring levels of nitrogen, phosphorous and potassium. The last number stands for potassium. Another option is to stop using commercial fertilizers all together and to begin using only organic matter to enrich the soil.
+        <br/> 3. Stop applying potassium-rich commercial fertilizer. Apply only commercial fertilizer that has a '0' in the final number field. Commercial fertilizers use a three number system for measuring levels of nitrogen, phosphorous and potassium. The last number stands for potassium. Another option is to stop using commercial fertilizers all together and to begin using only organic matter to enrich the soil.
 
-        <br/>4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.
+        <br/> 4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.
 
-        <br/>5. Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.
+        <br/> 5. Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.
 
-        <br/>6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.
+        <br/> 6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.
         """,
 
         'Klow': """The K value of your soil is low.
         <br/>Please consider the following suggestions:
 
-        <br/><br/>1. Mix in muricate of potash or sulphate of potash
-        <br/>2. Try kelp meal or seaweed
-        <br/>3. Try Sul-Po-Mag
-        <br/>4. Bury banana peels an inch below the soils surface
-        <br/>5. Use Potash fertilizers since they contain high values potassium
+        <br/><br/> 1. Mix in muricate of potash or sulphate of potash
+        <br/> 2. Try kelp meal or seaweed
+        <br/> 3. Try Sul-Po-Mag
+        <br/> 4. Bury banana peels an inch below the soils surface
+        <br/> 5. Use Potash fertilizers since they contain high values potassium
         """
     }
 
 crop_dictionary = { 0 : 'apple' ,  1: 'banana' , 2: 'blackgram' , 3: 'chickpea' , 4: 'coconut' , 5: 'coffee' , 6: 'cotton' , 7: 'grapes ', 8: 'jute' ,  9: 'kidneybeans' , 10: 'lentil' , 11: 'maize' , 12 : 'mango' , 13 : 'mothbeans' , 14 : 'mungbean' , 15 : 'muskmelon' , 16 : 'orange' , 17 : 'papaya' , 18 : 'pigeonpeas' ,  19 : 'pomegranate' , 20 : 'rice',  21 : 'watermelon' }
 
 
-# Loading crop recommendation model
+# Loading Crop Recommendation Model
 
 crop_recommendation_model_path = 'models/XGBoost.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
-
 # =========================================================================================
 
-# Custom functions for calculations
+# Weather API - For States and City Weather
 
 def weather_fetch(city_name):
     """
@@ -146,36 +145,31 @@ def weather_fetch(city_name):
 
 # ------------------------------------ FLASK APP -------------------------------------------------
 
-
 app = Flask(__name__)
 
 # render home page
 
-
 @ app.route('/')
 def home():
-    title = 'Harvestify - Home'
+    title = 'Agriculus - Home'
     return render_template('index.html', title=title)
 
 # render crop recommendation form page
 
-
 @ app.route('/crop-recommend')
 def crop_recommend():
-    title = 'Harvestify - Crop Recommendation'
+    title = 'Agriculus - Crop Recommendation'
     return render_template('crop.html', title=title)
 
 # render fertilizer recommendation form page
 
-
 @ app.route('/fertilizer')
 def fertilizer_recommendation():
-    title = 'Harvestify - Fertilizer Suggestion'
+    title = 'Agriculus - Fertilizer Suggestion'
 
     return render_template('fertilizer.html', title=title)
 
 # render disease prediction input page
-
 
 # ===============================================================================================
 
@@ -183,10 +177,9 @@ def fertilizer_recommendation():
 
 # render crop recommendation result page
 
-
 @ app.route('/crop-predict', methods=['POST'])
 def crop_prediction():
-    title = 'Harvestify - Crop Recommendation'
+    title = 'Agriculus - Crop Recommendation'
 
     if request.method == 'POST':
         N = int(request.form['nitrogen'])
@@ -214,12 +207,11 @@ def crop_prediction():
 
             return render_template('try_again.html', title=title)
 
-# render fertilizer recommendation result page
-
+# Fertilizer recommendation result page
 
 @ app.route('/fertilizer-predict', methods=['POST'])
 def fert_recommend():
-    title = 'Harvestify - Fertilizer Suggestion'
+    title = 'Agriculus - Fertilizer Suggestion'
 
     crop_name = str(request.form['cropname'])
     N = int(request.form['nitrogen'])
